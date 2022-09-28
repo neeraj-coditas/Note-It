c05df188fd7daa19471f614b4a4cd69fbcc9b415
Neeraj Raut <neeraj.raut@coditas.com>
2022-09-28 17:31:57 +0530

    Implemented Splash Screen

---
+++ app/src/main/java/com/example/noteit/SplashScreenFragment.kt
@@ -0,0 +1,39 @@
+package com.example.noteit
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            Runnable {
+                view.findNavController().navigate(R.id.action_splashScreenFragment2_to_homeFragment)
+            },
+            2000
+        )
+    }
+
+}
